7cb402b0aa3ba8199cbf0b598171f363cbd6d08d
Valeryi <ponyparser@gmail.com>
2023-10-02 19:12:23 +0100

    modal upload files refactor

--- self_hosting_machinery/webgui/static/components/modals/modal-upload-files.js
+++ self_hosting_machinery/webgui/static/components/modals/modal-upload-files.js
@@ -1,3 +1,57 @@
+
+let modal_html = `
+<div class="modal fade" id="updlg-modal-upload-files" tabindex="-1" role="dialog" aria-labelledby="updlg-modal-upload-files" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="updlg-upload-modal-label">Default Label</h5>
+      </div>
+      <div class="modal-body">
+        <!-- Nav tabs -->
+        <ul class="nav nav-tabs" id="updlg-nav-upload-files-tabs" role="tablist" style="margin-bottom: 15px">
+          <li class="nav-item">
+            <button class="updlg-nav-upload-files nav-link main-tab-button" id="updlg-nav-upload-files-tab-link" data-method="link" data-toggle="updlg-tab-upload-files-link-div">Upload by Link</button>
+          </li>
+          <li class="nav-item">
+            <button class="updlg-nav-upload-files nav-link main-tab-button" id="updlg-nav-upload-files-tab-input"  data-method="input" data-toggle="updlg-tab-upload-files-input-div">Choose file</button>
+          </li>
+        </ul>
+        <!-- Tab panes -->
+        <div class="tab-content">
+          <div class="updlg-pane-upload-files-modal tab-pane fade" id="updlg-tab-upload-files-link-div" role="tabpanel" aria-labelledby="updlg-tab-upload-files-link-div">
+            <div class="form-group">
+              <input type="text" class="form-control" id="updlg-upload-files-link" placeholder="Enter file URL">
+            </div>
+          </div>
+
+          <div class="updlg-pane-upload-files-modal tab-pane fade" id="updlg-tab-upload-files-input-div" role="tabpanel" aria-labelledby="updlg-tab-upload-files-input-div">
+            <div class="form-group">
+              <div class="mb-3">
+                <input type="file" id="updlg-upload-files-input" class="form-control">
+              </div>
+            </div>
+          </div>
+          <div id="updlg-file-upload-progress" class="progress d-none">
+          <div id="updlg-file-upload-progress-bar" class="progress-bar progress-bar-animated" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+          <div class="mb-3 form-text ssh-info"></div>
+          <div id="updlg-loaded_n_total" class="uploaded-total"></div>
+          <div>
+            <span id="updlg-upload-files-100-spinner" class="spinner-border spinner-border-sm" role="status" aria-hidden="true" hidden></span>
+            <span id="updlg-upload-files-status" class="uploaded-status sr-only"></span>
+        </div>
+        </div>
+      </div>
+      <div class="modal-footer" style="margin-top: 15px">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" id="updlg-upload-files-modal-submit">Submit</button>
+      </div>
+    </div>
+  </div>
+</div>
+`
+
+
 export async function init(
     insert_in_el,
     open_on_click_el,
@@ -9,21 +63,20 @@ export async function init(
     link_placeholder,
     input_help_text
 ) {
-    let req = await fetch('/components/modals/modal-upload-files.html');
-    insert_in_el.innerHTML = await req.text();
-    insert_in_el.querySelector('#upload-modal-label').innerHTML = modal_label;
+    insert_in_el.innerHTML = modal_html;
+    insert_in_el.querySelector('#updlg-upload-modal-label').innerHTML = modal_label;
 
     if (default_tab === 'link') {
-        insert_in_el.querySelector('#nav-upload-files-tab-link').classList.add('active', 'main-active')
-        insert_in_el.querySelector('#tab-upload-files-link-div').classList.add('show', 'active')
+        insert_in_el.querySelector('#updlg-nav-upload-files-tab-link').classList.add('active', 'main-active')
+        insert_in_el.querySelector('#updlg-tab-upload-files-link-div').classList.add('show', 'active')
     } else if (default_tab === 'input') {
-        insert_in_el.querySelector('#nav-upload-files-tab-input').classList.add('active','main-active')
-        insert_in_el.querySelector('#tab-upload-files-input-div').classList.add('show', 'active')
+        insert_in_el.querySelector('#updlg-nav-upload-files-tab-input').classList.add('active','main-active')
+        insert_in_el.querySelector('#updlg-tab-upload-files-input-div').classList.add('show', 'active')
     } else {
         console.log(`default tab ${default_tab} is not implemented!`);
     }
     if (link_placeholder) {
-        insert_in_el.querySelector('#upload-files-link').placeholder = link_placeholder;
+        insert_in_el.querySelector('#updlg-upload-files-link').placeholder = link_placeholder;
     }
     if (input_help_text) {
         insert_in_el.querySelector('.ssh-info').innerHTML = input_help_text;
@@ -46,7 +99,7 @@ class UploadFilesModalEvents {
         submit_input_endpoint,
         text_on_progress_done,
     ) {
-        this.file_modal = document.getElementById('modal-upload-files');
+        this.file_modal = document.getElementById('updlg-modal-upload-files');
 
         this.nav_btn_click_events();
         this.submit_event(submit_link_endpoint, submit_input_endpoint, text_on_progress_done);
@@ -55,8 +108,8 @@ class UploadFilesModalEvents {
     nav_btn_click_events() {
         const file_modal = this.file_modal;
 
-        const btns_nav_upload_files = file_modal.querySelectorAll('button.nav-upload-files')
-        const panes_upload_files = file_modal.querySelectorAll('.pane-upload-files-modal');
+        const btns_nav_upload_files = file_modal.querySelectorAll('button.updlg-nav-upload-files')
+        const panes_upload_files = file_modal.querySelectorAll('.updlg-pane-upload-files-modal');
         btns_nav_upload_files.forEach(
             el => el.addEventListener('click', () => {
                 if (!el.classList.contains('active')) {
@@ -72,7 +125,7 @@ class UploadFilesModalEvents {
     submit_event(submit_link_endpoint, submit_input_endpoint, text_on_progress_done) {
         const file_modal = this.file_modal;
         function get_upload_method() {
-            const btns = file_modal.querySelectorAll('button.nav-upload-files')
+            const btns = file_modal.querySelectorAll('button.updlg-nav-upload-files')
             for (const btn of btns) {
                 if (btn.classList.contains('active')) {
                     return btn.dataset.method;
@@ -80,7 +133,7 @@ class UploadFilesModalEvents {
             }
         }
 
-        const upload_files_submit = this.file_modal.querySelector('#upload-files-modal-submit')
+        const upload_files_submit = this.file_modal.querySelector('#updlg-upload-files-modal-submit')
 
         upload_files_submit.addEventListener('click', () => {
             const upload_method = get_upload_method();
@@ -97,54 +150,53 @@ class UploadFilesModalEvents {
 
 
     reset_modal_fields() {
-        const file_modal = document.getElementById('modal-upload-files');
+        const file_modal = document.getElementById('updlg-modal-upload-files');
 
-        file_modal.querySelector('#upload-files-input').value = '';
-        file_modal.querySelector('#upload-files-modal-submit').disabled = false;
-        file_modal.querySelector('#nav-upload-files-tab-link').disabled = false;
-        file_modal.querySelector('#upload-files-link').disabled = false;
-        file_modal.querySelector('#upload-files-input').disabled = false;
-        file_modal.querySelector('#file-upload-progress-bar').setAttribute('aria-valuenow', '0');
-        file_modal.querySelector('#file-upload-progress').classList.toggle('d-none');
-        file_modal.querySelector('#upload-files-modal-submit').classList.remove('d-none');
-        file_modal.querySelector('#loaded_n_total').innerHTML = "";
-        file_modal.querySelector('#upload-files-status').innerHTML = "";
-        file_modal.querySelector('#upload-files-link').value = "";
-        file_modal.querySelector('#upload-files-100-spinner').hidden = true;
-        file_modal.querySelector('#nav-upload-files-tab-input').disabled = false;
+        file_modal.querySelector('#updlg-upload-files-input').value = '';
+        file_modal.querySelector('#updlg-upload-files-modal-submit').disabled = false;
+        file_modal.querySelector('#updlg-nav-upload-files-tab-link').disabled = false;
+        file_modal.querySelector('#updlg-upload-files-link').disabled = false;
+        file_modal.querySelector('#updlg-upload-files-input').disabled = false;
+        file_modal.querySelector('#updlg-file-upload-progress-bar').setAttribute('aria-valuenow', '0');
+        file_modal.querySelector('#updlg-file-upload-progress').classList.toggle('d-none');
+        file_modal.querySelector('#updlg-upload-files-modal-submit').classList.remove('d-none');
+        file_modal.querySelector('#updlg-loaded_n_total').innerHTML = "";
+        file_modal.querySelector('#updlg-upload-files-status').innerHTML = "";
+        file_modal.querySelector('#updlg-upload-files-link').value = "";
+        file_modal.querySelector('#updlg-upload-files-100-spinner').hidden = true;
+        file_modal.querySelector('#updlg-nav-upload-files-tab-input').disabled = false;
     }
 
     hide_modal() {
-        bootstrap.Modal.getOrCreateInstance(document.getElementById('modal-upload-files')).hide();
+        bootstrap.Modal.getOrCreateInstance(document.getElementById('updlg-modal-upload-files')).hide();
     }
 
     show_modal() {
-        bootstrap.Modal.getOrCreateInstance(document.getElementById('modal-upload-files')).show();
+        bootstrap.Modal.getOrCreateInstance(document.getElementById('updlg-modal-upload-files')).show();
     }
 
     process_now_update_until_finished(upload_method) {
-        const file_modal = document.getElementById('modal-upload-files');
-        const process_button = file_modal.querySelector('#upload-files-modal-submit');
+        const file_modal = document.getElementById('updlg-modal-upload-files');
+        const process_button = file_modal.querySelector('#updlg-upload-files-modal-submit');
         process_button.disabled = true;
         process_button.dataset.loading = 'true';
         if (upload_method === 'link') {
-            file_modal.querySelector('#nav-upload-files-tab-input').disabled = true;
-            file_modal.querySelector('#upload-files-link').disabled = true;
-            file_modal.querySelector('#upload-files-100-spinner').hidden = false;
-            file_modal.querySelector('#upload-files-status').innerHTML = 'Uploading file. Please wait...'
+            file_modal.querySelector('#updlg-nav-upload-files-tab-input').disabled = true;
+            file_modal.querySelector('#updlg-upload-files-link').disabled = true;
+            file_modal.querySelector('#updlg-upload-files-100-spinner').hidden = false;
+            file_modal.querySelector('#updlg-upload-files-status').innerHTML = 'Uploading file. Please wait...'
         } else if (upload_method === 'input') {
-            file_modal.querySelector('#nav-upload-files-tab-link').disabled = true;
-            file_modal.querySelector('#upload-files-input').disabled = true;
+            file_modal.querySelector('#updlg-nav-upload-files-tab-link').disabled = true;
+            file_modal.querySelector('#updlg-upload-files-input').disabled = true;
         }
     }
 
     upload_file(endpoint, text_on_progress_done) {
         const file_modal = this.file_modal;
-        const file_input = file_modal.querySelector('#upload-files-input');
-        const file_upload_progress = file_modal.querySelector('#file-upload-progress');
-        const progress_bar = file_modal.querySelector('#file-upload-progress-bar');
-        const upload_files_status = file_modal.querySelector('#upload-files-status');
-        // const modal_submit = file_modal.querySelector('#upload-files-modal-submit');
+        const file_input = file_modal.querySelector('#updlg-upload-files-input');
+        const file_upload_progress = file_modal.querySelector('#updlg-file-upload-progress');
+        const progress_bar = file_modal.querySelector('#updlg-file-upload-progress-bar');
+        const upload_files_status = file_modal.querySelector('#updlg-upload-files-status');
 
         const process_now_update_until_finished = this.process_now_update_until_finished;
         const hide_modal = this.hide_modal;
@@ -152,14 +204,14 @@ class UploadFilesModalEvents {
 
         function progressHandler(event) {
             process_now_update_until_finished('input');
-            file_modal.querySelector('#loaded_n_total').innerHTML = "Uploaded " + event.loaded + " bytes of " + event.total;
+            file_modal.querySelector('#updlg-loaded_n_total').innerHTML = "Uploaded " + event.loaded + " bytes of " + event.total;
             let percent = (event.loaded / event.total) * 100;
             progress_bar.setAttribute('aria-valuenow', Math.round(percent).toString());
             progress_bar.style.width = Math.round(percent).toString() + "%";
             upload_files_status.innerHTML = Math.round(percent).toString() + "% uploaded... please wait";
             if (Math.round(percent) >= 100) {
                 upload_files_status.innerHTML = text_on_progress_done;
-                file_modal.querySelector('#upload-files-100-spinner').hidden = false;
+                file_modal.querySelector('#updlg-upload-files-100-spinner').hidden = false;
             }
         }
 
@@ -210,7 +262,7 @@ class UploadFilesModalEvents {
             file_modal.querySelector('#status-url').innerHTML = error.message;
         }
 
-        const file_input = file_modal.querySelector('#upload-files-link');
+        const file_input = file_modal.querySelector('#updlg-upload-files-link');
         if (!file_input || file_input.value === '') {
             return;
         }
@@ -249,7 +301,7 @@ class UploadFilesModalEvents {
                     this.hide_modal();
                     }).catch(
                         error => {
-                        file_modal.querySelector('#upload-files-status').innerHTML = error.message;
+                        file_modal.querySelector('#updlg-upload-files-status').innerHTML = error.message;
                 });
     }
 }
